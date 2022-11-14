f378e2b89b1e96e5ea7383768b7ca8773876c76a
Eunjeong Sim <Eunjeong.Sim@jdplc.com>
2022-11-14 14:30:35 +0000

    day4

---
+++ 04_Day_Conditionals/04_day pracitice.js
@@ -0,0 +1,64 @@
+
+
+// let isRaining = true
+// if (isRaining){
+//     console.log('remember to take your rain coat.')
+// }
+
+// let isNotRaining = false
+// if (isNotRaining){
+//     console.log('remember to take your rain coat.')
+// }
+
+// let num = 3 
+// if ( num > 0 ){
+//     console.log(`${num} is a positive number`)
+// } else {
+//     console.log(`${Number} is a negative number`)
+// }
+
+// let isRaining = false
+// if (isRaining) {
+//     console.log('You need a rain coat.')
+// } else {
+//     console.log('No need for a rain coat')
+// }
+
+// let num = -3
+// if ( num> 0 ){
+//     console.log(`${num} is a postive number`)
+// } else {
+//     console.log(`${num} is a negative number`)
+// }
+
+let weather = 'cloudy'
+switch (weather){
+    case 'rainy':
+        console.log('You need a rain coat')
+        break
+    case 'cloudy':
+        console.log('It might be cold, you need a jacket')
+        break
+    case 'Sunny':
+        console.loog('Go out freely')
+        break
+    }
+
+    // let num = prompt('Enter number')
+    // switch (true) {
+    //     case num > 0:
+    //     console.log('Number is positive')
+    //     break;
+    //     case num == 0:
+    //     console.log('Numbers is zero');
+    //     break;
+    // }
+
+    let isRaining = true
+    isRaining 
+    ? console.log('You need a rain coat')
+    : console.log('No need for a rain coat')
+
+    // EXERCISE ? let's go : stop // 
+
+    
