356c1aca5db99c6ea98077b223dc25238ceda4af
Eunjeong Sim <Eunjeong.Sim@jdplc.com>
2022-11-17 16:30:27 +0000

    day1

---
+++ 06_Day_Loops/06_day_starter/example.js
@@ -0,0 +1,100 @@
+// // For loop structure
+// const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+// const newArr = []
+// for(let i = 0; i < countries.length; i++){
+//     newArr.push(countries[i].toUpperCase())
+// }
+// console.log(newArr)
+
+// // Adding all elements in the array
+
+// const numbers = [1,2,3,4,5]
+// let sum = 0 
+// for(let i = 0; i < numbers.length; i++){
+//     sum = sum + numbers[i]
+// }
+// console.log(sum)
+
+// // Creating a new array based on the existing array
+
+// const numbers2 = [1,2,3,4,5]
+// const newArr2 = []
+// let sum2 = 0 
+// for(let i = 0; i < numbers2.length; i++){
+//  newArr2.push(numbers2[i] ** 2)
+// }
+
+// console.log(newArr2)
+
+// // while loop
+
+// let i = 0
+// while ( i <= 5){
+//     console.log(i)
+//     i++
+// }
+
+// // do while loop
+
+// let j = 0 
+// do { console.log(j)
+//      j++
+// } while ( j <= 5)
+
+// for of loop
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// for (const num of numbers) {
+//   console.log(num*num)
+// }
+
+// let sum = 0
+// for (const num of numbers){
+//     sum = sum + num
+// }
+
+// const webTechs = [
+//     'HTML',
+//     'CSS',
+//     'JavaScript',
+//     'React',
+//     'Redux',
+//     'Node',
+//     'MongoDB'
+//   ]
+
+//   for (const tech of webTechs){
+//     console.log(tech.toUpperCase())
+//   }
+
+//   for ( const tech of webTechs){
+//     console.log(tech[0])
+//   }
+
+// for of loop
+
+//   const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+//   const newArr = []
+//   for(const country of countries){
+//     newArr.push(country.toUpperCase())
+//   }
+
+// break
+
+// for (let i = 0 ; i <= 5; i++){
+//     if(i == 3){
+//         break
+//     }
+//     console.log(i)
+// }
+
+// countinue
+// We use the keyword continue to skip a certain iterations.
+// for (let i = 0; i <= 5; i++){
+//     if(i == 3){
+//         continue
+//     }
+//     console.log(i)
+// }
+
---
+++ 06_Day_Loops/06_day_starter/exercise.js
@@ -0,0 +1,43 @@
+// 1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// 2. Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+// let i = 0;
+// while (i < 10) {
+//     i++;
+//     console.log(i);
+// }
+
+// 3. Iterate 0 to n using for loop
+
+let i = 0;
+do {
+    i++;
+    console.log(i)
+} while (i < 10)
+
+// 4. Write a loop that makes the following pattern using console.log()
+
+let str = "";
+for (let i = 0; i < 7; i++) {
+    str += "#";
+    console.log(str)
+}
+
+// 5. Use loop to print the following pattern
+
+for ( i=0; i <=10; i++){
+    console.log(`${i} * ${i} = {i*i}`)
+}
+
+// 6. Using loop print the following pattern
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${i} ${i ** 2} ${i ** 3}`)
+}
+
+// 7. Use for loop to iterate from 0 to 100 and print only even numbers
